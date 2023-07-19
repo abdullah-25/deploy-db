faf5848650dbd540d57b1deacfab5162d21930d9
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-07-19 12:54:48 -0400

    add cors middleware

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
+app.use(cors());
 
-const warehouseRoutes = require('./routes/warehouseRoute');
-const inventoryRoutes = require('./routes/inventoryRoute');
+const warehouseRoutes = require("./routes/warehouseRoute");
+const inventoryRoutes = require("./routes/inventoryRoute");
 
-app.get('/', (_req, res) => {
+app.get("/", (_req, res) => {
   res.send("Welcome to my API");
 });
 
-app.use('/warehouses', warehouseRoutes);
-app.use('/inventories', inventoryRoutes);
+app.use("/warehouses", warehouseRoutes);
+app.use("/inventories", inventoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
